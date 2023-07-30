2c1021d183e0c2f90a1f2907a8dd39ea80eeaa73
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-30 13:08:00 +0200

    feat: static header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { AiFillGithub } from "react-icons/ai";
+import { FiMenu } from "react-icons/fi";
+
+import { T3Logo } from "~/components/T3Logo";
+
+export function Header() {
+  return (
+    <header className="container absolute flex items-center gap-4 bg-transparent px-4 py-4 pb-20">
+      <div className="w-14">
+        <T3Logo />
+      </div>
+      <div className="ml-auto">
+        <AiFillGithub size={40} />
+      </div>
+      <FiMenu size={40} />
+    </header>
+  );
+}
